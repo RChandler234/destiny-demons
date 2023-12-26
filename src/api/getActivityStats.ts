517677f3bc5ccdc29c679ce63222e0e7931c02dd
RChandler234 <29521172+RChandler234@users.noreply.github.com>
2023-12-25 19:49:49 -0600

    parameters bug

--- src/api/getActivityStats.ts
+++ src/api/getActivityStats.ts
@@ -12,11 +12,11 @@ export const getRaidActivityInfo = (
   player: Player,
   characterId: string
 ) => {
-  const url = `https://www.bungie.net/Platform/Destiny2/3/Account/${player.memberShipId}/Character/${characterId}/Stats/Activities/?count=250&mode=4&page=${page}/`;
+  const url = `https://www.bungie.net/Platform/Destiny2/3/Account/${player.memberShipId}/Character/${characterId}/Stats/Activities/?count=250&mode=4&page=${page}`;
   return axios.get(url, config);
 };
 
 export const getCarnageReport = (activityId: number) => {
-  const url = `https://www.bungie.net/Platform/Destiny2/Stats/PostGameCarnageReport/${activityId}/`;
+  const url = `https://www.bungie.net/Platform/Destiny2/Stats/PostGameCarnageReport/${activityId}`;
   return axios.get(url, config);
 };
